9f63a272d870cef75208ad742aed1eea81631682
casya86 <casya86@mail.ru>
2015-07-30 20:00:01 +0300

    add категирии в апдейт нотесов

--- views/notes/_form.php
+++ views/notes/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use app\models\Categories;
 
-use kartik\tree\TreeView;
+//use kartik\tree\TreeView;
 use kartik\tree\TreeViewInput;
 
 
@@ -18,24 +18,25 @@ use kartik\tree\TreeViewInput;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-    
-    
+
     <?= TreeViewInput::widget([
         'name' => 'kvTreeInput',
-        'value' => '1,2,3', // preselected values
+        //'value' => '1,2,3', // preselected values        
         'query' => Categories::find()->addOrderBy('root, lft'),
         'headingOptions' => ['label' => 'Categories'],
         'rootOptions' => ['label'=>'<i class="fa fa-tree text-success"></i>'],
-        'fontAwesome' => true,
+        //'fontAwesome' => true,
         'asDropdown' => true,
-        'multiple' => true,
+        'multiple' => false,
         'options' => ['disabled' => false]
     ]);?>
     
 
-    <?= $form->field($model, 'created')->textInput() ?>
+    <?php //$form->field($model, 'created')->textInput() ?>
 
-    <?= $form->field($model, 'id_file')->textInput() ?>
+    <?php //$form->field($model, 'id_file')->textInput() ?>
+
+    <br>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
